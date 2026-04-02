7be5716cba2d636a8aa03bf1dbce720f78082d50
arghyaguha012 <arghyaguha012@gmail.com>
2026-04-03 01:12:34 +0530

    auth-service completed

--- auth-service/src/app.ts
+++ auth-service/src/app.ts
@@ -11,15 +11,24 @@ import { config } from "./config"
 import init from "./init";
 import { indexRouter } from "./routes/index.route";
 import authRouter from "./routes/auth.route";
+import {errorHandler} from "./middleware/error.middleware"
+import { corsMiddleware } from "./middleware/cors.middleware";
+import { verifyToken } from "./middleware/auth.middleware";
+import { reqLogger } from "./middleware/req.middlware";
+import { setupGracefulShutdown } from "./utils/shutdown";
 
 const app = express()
 app.use(helmet())
-
+app.use(corsMiddleware);
+app.use(reqLogger)
 app.use(express.json())
-
+app.use(verifyToken)
 
 app.use("/", indexRouter)
 app.use("/api/v1/auth", authRouter)
+app.use(errorHandler)
+
+
 
 AppDataSource.initialize()
   .then(async () => {
@@ -28,8 +37,9 @@ AppDataSource.initialize()
     const server = app.listen(config.PORT, () => {
       logger.info(`${config.SERVICE_NAME} running on http://localhost:${config.PORT}`)
     })
+    setupGracefulShutdown(server);
   })
   .catch((err) => {
     logger.error('Failed to start server', err);
-    //process.exit(1);
+   
   })
