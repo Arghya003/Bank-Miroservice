128c85be26f3e042a9cbdb7f3f9493aa669c8c2f
Arghya Guha <96729154+Arghya003@users.noreply.github.com>
2026-04-07 01:09:46 +0530

    Account service (#5)
    * public urls for auth added
    
    * account-service setup complete
    
    ---------
    
    Co-authored-by: arghyaguha012 <arghyaguha012@gmail.com>

--- account-service/src/middlewares/auth.middleware.ts
+++ account-service/src/middlewares/auth.middleware.ts
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 import { config } from '../config';
 import redis from '../config/redis';
 
-const publicRoutes = ['/api/v1/auth/login/', '/api/v1/auth/register/','/','health/'];
+const publicRoutes = ['/', '/health'];
 
 export const verifyToken = (
   req: Request,
@@ -13,7 +13,6 @@ export const verifyToken = (
   if (publicRoutes.includes(req.path)) {
     return next();
   }
-  
 
   const token = req.headers['authorization']?.split(' ')[1];
 
---
+++ account-service/src/router/account.route.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+const accountRouter = Router();
+
+
+accountRouter.post('/',);
+accountRouter.get('/', );
+accountRouter.delete(
+  '/:accountNumber',
+  );
+
+accountRouter.post(
+  '/internal/transaction',
+    
+);
+
+export { accountRouter };
